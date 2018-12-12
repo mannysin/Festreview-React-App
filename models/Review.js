f18b97bd0a2df154b3853ebf9225f7c0fcb902a9
mannysin <sinclair2610@gmail.com>
2018-12-12 11:41:08 -0500

    created first Review model

---
+++ models/Review.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+
+const reviewSchema = new Schema({
+  author: String,
+  overallRating: Number,
+  review: String,
+  soundRating: Number,
+  artRating: Number,
+  foodRating: Number,
+  venueRating: Number,
+  stageRating: Number,
+  activitiesRating: Number,
+  vibeRating: Number,
+}, {
+  timestamps: true
+});
+
+const Review = mongoose.model("Review", reviewSchema);
+
+module.exports = Review;
