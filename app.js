35a679464d46cc3f2c76fdfe55edc52c6863eeca
mannysin <sinclair2610@gmail.com>
2018-12-18 15:06:38 -0500

    deployment variables

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ require('./config/passport')
 
 
 mongoose
-  .connect('mongodb://localhost/festreview-api', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
